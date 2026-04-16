be50348614539b861e8cc53367e5cf1f79547029
Shivshankar Shrikant Pirgonde <shpirgonde@gmail.com>
2026-04-16 16:50:59 +0530

    login component creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', loadComponent: () => import('./features/login/login').then(m => m.Login) },
+];
