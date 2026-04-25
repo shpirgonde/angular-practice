9dfa06a6847817ff5e4a1b7a26a6e1e1d7c554cb
Shivshankar Shrikant Pirgonde <shpirgonde@gmail.com>
2026-04-25 22:00:07 +0530

    commit for modal testing

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss',
 })
 export class Login {
+  private dialog = viewChild<ElementRef<HTMLDialogElement>>('statusDialog');
 
+  statusMessage = signal('');
+
+  onLogin(event: Event) {
+    event.preventDefault();// When you click a "Submit" button, the browser naturally reloads the page. Using preventDefault() stops that reload so you can validate inputs or send data via
+    const isSuccess = Math.random() > 0.5; // Simulate a login success or failure randomly
+
+    if (isSuccess) {
+      this.statusMessage.set('Login successful!');
+    }
+    else {
+      this.statusMessage.set('Error: Invalid credentials. Please try again.');
+    }
+
+    //Open the native modal
+
+    this.dialog()?.nativeElement.showModal();
+  }
+
+  closeModal() {
+    this.dialog()?.nativeElement.close();
+  }
 }
