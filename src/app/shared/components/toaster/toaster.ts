b611b740497735358646169f16f02be76edd0ad1
Shivshankar Shrikant Pirgonde <shpirgonde@gmail.com>
2026-05-01 10:10:50 +0530

    custom toaster

---
+++ src/app/shared/components/toaster/toaster.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgClass } from "../../../../../node_modules/@angular/common/common_module.d";
+
+@Component({
+  selector: 'app-toaster',
+  imports: [NgClass],
+  templateUrl: './toaster.html',
+  styleUrl: './toaster.scss',
+})
+export class Toaster {
+
+}
