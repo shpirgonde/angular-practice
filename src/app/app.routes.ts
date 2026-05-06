b22f8f1d2fdd5f54f3eb5a5fd0b1438b785175cf
Shivshankar Shrikant Pirgonde <shpirgonde@gmail.com>
2026-05-06 21:48:19 +0530

    Sign up form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    
+
     {
         path: 'login',
         loadComponent: () => import('./features/login/login').then(m => m.Login)
     },
 
-    
-    { path: 'signup', loadComponent: () => import('./features/signup/signup').then(m => m.Signup) },
+
+    {
+        path: 'signup',
+        loadComponent: () => import('./features/signup/signup').then(m => m.Signup)
+    },
 ];
--- src/app/features/signup/signup.ts
+++ src/app/features/signup/signup.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
